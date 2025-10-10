c66fbb29912cfac9c9a4747625b01b244923b662
ILGeneral <igeneral@gbox.adnu.edu.ph>
2025-10-10 12:04:05 +0800

    Removed react native dotenv (Won't work with Babel)

--- Culina/Frontend/babel.config.js
+++ Culina/Frontend/babel.config.js
@@ -4,13 +4,6 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       "expo-router/babel",
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-        },
-      ],
       "react-native-reanimated/plugin",
     ],
   };
--- Culina/Frontend/types/env.d.ts
+++ Culina/Frontend/types/env.d.ts
@@ -1,7 +1,9 @@
-declare module "@env" {
-  export const CLARIFAI_PAT: string;
-  export const CLARIFAI_USER_ID: string;
-  export const CLARIFAI_APP_ID: string;
-  export const CLARIFAI_MODEL_ID: string;
-  export const CLARIFAI_MODEL_VERSION_ID: string;
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_CLARIFAI_PAT: string;
+    EXPO_PUBLIC_CLARIFAI_USER_ID: string;
+    EXPO_PUBLIC_CLARIFAI_APP_ID: string;
+    EXPO_PUBLIC_CLARIFAI_MODEL_ID: string;
+    EXPO_PUBLIC_CLARIFAI_MODEL_VERSION_ID: string;
+  }
 }
